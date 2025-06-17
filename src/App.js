c2419728fd58847a1ac082105a5b648738a92bbe
fukushi01110111 <kaitofukushima82@gmail.com>
2025-06-17 21:41:57 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            こんにちは！初めてのReactアプリです！
+          </p>
+          <p>
+            現在の時刻: {new Date().toLocaleString()}
+          </p>
+          Reactを学ぼう
         </a>
       </header>
     </div>
